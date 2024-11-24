4f0cbb6a86658238eb8ed65f05a47f3703b62ea5
Zhangmeili667 <3407733426@qq.com>
2024-11-24 15:08:51 +0800

    第二次修改后端，增加导入导出接口

--- ContactBackend1/src/main/java/com/yusheng/contactbackend1/controller/PeopleController.java
+++ ContactBackend1/src/main/java/com/yusheng/contactbackend1/controller/PeopleController.java
@@ -96,13 +96,7 @@ public class PeopleController {
 
     @GetMapping("/download")
     public void download(HttpServletResponse response) throws IOException {
-//        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
-//        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-//        response.setCharacterEncoding("utf-8");
-//        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
-//        String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
-//        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
-//        EasyExcel.write(response.getOutputStream(), People.class).sheet("模板").doWrite(peopleService.list());
+
         List<People> userInfoList = peopleService.list();
         EasyExcelUtils.download("用户列表", userInfoList);
 
@@ -141,11 +135,5 @@ public class PeopleController {
     }
 
 
-//    @GetMapping("/selectById")
-//    public Result selectById(@RequestParam Integer id){
-//
-//        People people = peopleService.selecetById(id);
-//        return Result.success(people);
-//    }
 
 }
